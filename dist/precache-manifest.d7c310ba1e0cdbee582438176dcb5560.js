85409e0a3c05e4565268abb80fd80f31634c5823
azertypow <azertypow@gmail.com>
2021-04-13 23:09:03 +0200

    build: v0.1

---
+++ dist/precache-manifest.d7c310ba1e0cdbee582438176dcb5560.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1738cc91f352e8e9db",
+    "url": "/css/about.a81ffcf6.css"
+  },
+  {
+    "revision": "05c63d74ab4a88cfe33c",
+    "url": "/css/app.fa29bcda.css"
+  },
+  {
+    "revision": "426c4bc915d444663ebb",
+    "url": "/css/contact.6baa79d9.css"
+  },
+  {
+    "revision": "897294498bc1fbcf8b71",
+    "url": "/css/home.b1f351d0.css"
+  },
+  {
+    "revision": "35574c0a15fac9ff4dd8",
+    "url": "/css/shop.ccfe0b4a.css"
+  },
+  {
+    "revision": "4c59e868e99596d1a52d292d98706d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb1738cc91f352e8e9db",
+    "url": "/js/about.534feca2.js"
+  },
+  {
+    "revision": "05c63d74ab4a88cfe33c",
+    "url": "/js/app.41a8de37.js"
+  },
+  {
+    "revision": "c3682893c36feb160e52",
+    "url": "/js/chunk-vendors.1c92fb26.js"
+  },
+  {
+    "revision": "426c4bc915d444663ebb",
+    "url": "/js/contact.9443e265.js"
+  },
+  {
+    "revision": "897294498bc1fbcf8b71",
+    "url": "/js/home.c5ee743e.js"
+  },
+  {
+    "revision": "35574c0a15fac9ff4dd8",
+    "url": "/js/shop.04a67b51.js"
+  },
+  {
+    "revision": "ad64b18fd4ea1b02a1205eac04470b9e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
